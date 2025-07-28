14b86df08849c8eaaca1af0e926ff2b42d91739d
ninadrathod <ninadrathod267@gmail.com>
2025-07-28 20:04:22 +0530

    creating abstract for library functions

---
+++ main.py
@@ -0,0 +1,18 @@
+from transactions import *
+from linear_hashing_lib import *
+
+# create user input
+input_array = []
+for i in range (1,60000): 
+    input_array.append([i,sale_amount(),customer_name(),category()])
+
+
+# insert data in hash
+for transaction in input_array:
+
+
+
+
+   
+
+
---
+++ transactions.py
@@ -0,0 +1,11 @@
+def sale_amount():
+    # write a logic to return a random amount between 1 and 80,000
+    pass
+
+def customer_name():
+    # write a logic to return a customer name with 3 random charaters
+    pass
+
+def category():
+    # write a logic to return a random integer between 1 and 1500
+    pass
